aa3fb0c052434a88d29f602aa57e7469fb7982f7
mat <mat@student.tudelft.nl>
2019-12-20 10:20:37 +0300

    Creating user_id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -76,6 +76,6 @@ class TasksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     #
     def task_params
-      params.require(:task).permit(:subj, :descript, :status)
+      params.require(:task).permit(:subj, :descript, :status, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_213554) do
+ActiveRecord::Schema.define(version: 2019_12_20_065800) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "subj"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_213554) do
     t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
 end
